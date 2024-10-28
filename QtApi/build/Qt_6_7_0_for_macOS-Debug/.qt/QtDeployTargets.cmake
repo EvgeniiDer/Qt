27be8406ecef6cde5682799550b4aba4bea42fff
EvgeniiDer <128982133+EvgeniiDer@users.noreply.github.com>
2024-10-28 08:27:23 +0300

    Delete

--- QtApi/build/Qt_6_7_0_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,1 +0,0 @@
-set(__QT_DEPLOY_TARGET_QtApji_FILE /Users/evgenii/Project/Qt/QtApji/build/Qt_6_7_0_for_macOS-Debug/QtApji)
--- QtApi/head.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEAD_H
-#define HEAD_H
-
-#include<QCoreApplication>
-#include<QTextStream>
-#include<QWidget>
-#include<QMessageBox>
-
-#endif // HEAD_H
