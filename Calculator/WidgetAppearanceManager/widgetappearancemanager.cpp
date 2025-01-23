33edaa84473ad31304f834f6f133058bad4ad8d2
EvgeniiDer <128982133+EvgeniiDer@users.noreply.github.com>
2025-01-23 15:00:09 +0300

    HomeWork

--- Calculator/WidgetAppearanceManager/widgetappearancemanager.cpp
+++ Calculator/WidgetAppearanceManager/widgetappearancemanager.cpp
@@ -5,6 +5,11 @@ WidgetAppearanceManager::WidgetAppearanceManager(QObject* parent) : QObject(pare
 
 
 }
+QString WidgetAppearanceManager::settingsKey(const QString& widgetName, const QString& parameter)
+{
+    return widgetName + "/" + parameter;
+}
+
 void WidgetAppearanceManager::saveGeometry(const QWidget* widget, const QString& widgetName)
 {
     if(!widget)
@@ -14,25 +19,59 @@ void WidgetAppearanceManager::saveGeometry(const QWidget* widget, const QString&
     settings.setValue(settingsKey(widgetName, "size"), widget->size());
 
 }
-bool WidgetAppearanceManager::restoreGeometry(const QWidget* wodget, const QString& widgetName)
+bool WidgetAppearanceManager::restoreGeometry(QWidget* widget, const QString& widgetName)
 {
+    if(!widget)
+        return false;
+    QPoint pos = settings.value(settingsKey(widgetName, "pos")).toPoint();
+    QSize size = settings.value(settingsKey(widgetName, "size")).toSize();
 
+    if(!pos.isNull() && !size.isNull())
+    {
+        widget->move(pos);
+        widget->resize(size);
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
-void WidgetAppearanceManager::saveFont(const QWidget& widget, const QString& widgetName)
+void WidgetAppearanceManager::saveFont(const QWidget* widget, const QString& widgetName)
 {
-
+    if(!widget)
+        return;
+    settings.setValue(settingsKey(widgetName, "font"), widget->font());
 }
-bool WidgetAppearanceManager::restoreFont(const QWidget& widget, const QString& widgetName)
+bool WidgetAppearanceManager::restoreFont(QWidget* widget, const QString& widgetName)
 {
-
+    if(!widget)
+        return false;
+    QFont font = settings.value(settingsKey(widgetName, "font"), QFont()).value<QFont>();//как я понял извлекаем безопасно шрифет
+    if(font != QFont())//проверка на пустой шрифт
+    {
+        widget->setFont(font);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
-void WidgetAppearanceManager::saveStyle(const QWidget& widget, const QString& widgetName)
+void WidgetAppearanceManager::saveStyleSheet(const QWidget* widget, const QString& widgetName)
 {
-
+    if(!widget)
+        return;
+    settings.setValue(settingsKey(widgetName, "styleSheet"), widget->styleSheet());
 }
-bool WidgetAppearanceManager::restoreStyle(const QWidget& widget, const QString& widgetName)
+bool WidgetAppearanceManager::restoreStyleSheet(QWidget* widget, const QString& widgetName)
 {
-
+    if(!widget)
+        return false;
+    QString styleSheet = settings.value(settingsKey(widgetName, "styleSheet"), QString()).toString();
+    if(!styleSheet.isNull())
+    {
+        widget->setStyleSheet(styleSheet);
+    }
 }
--- Calculator/WidgetAppearanceManager/widgetappearancemanager.h
+++ Calculator/WidgetAppearanceManager/widgetappearancemanager.h
@@ -12,13 +12,13 @@ public:
     explicit WidgetAppearanceManager(QObject* parent = nullptr);
 
     void saveGeometry(const QWidget* widget, const QString& widgetName);
-    bool restoreGeometry(const QWidget* wodget, const QString& widgetName);
+    bool restoreGeometry(QWidget* widget, const QString& widgetName);
 
-    void saveFont(const QWidget& widget, const QString& widgetName);
-    bool restoreFont(const QWidget& widget, const QString& widgetName);
+    void saveFont(const QWidget* widget, const QString& widgetName);
+    bool restoreFont(QWidget* widget, const QString& widgetName);
 
-    void saveStyle(const QWidget& widget, const QString& widgetName);
-    bool restoreStyle(const QWidget& widget, const QString& widgetName);
+    void saveStyleSheet(const QWidget* widget, const QString& widgetName);
+    bool restoreStyleSheet(QWidget* widget, const QString& widgetName);
 private:
     QString settingsKey(const QString& widgetName, const QString& parameter);
     QSettings settings;
